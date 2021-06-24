d161fd110049830544758d9bb11b9ea12f2449b7
Elmar Angao <32320010+eangao@users.noreply.github.com>
2021-06-24 18:50:39 +0800

    create react back end

---
+++ backend/src/main/java/com/okta/developer/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.okta.developer.repository;
+
+import com.okta.developer.domain.Authority;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+/**
+ * Spring Data R2DBC repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
